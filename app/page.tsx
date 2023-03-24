03e8eea207eea44bf48f0ab70fb3d9ef4142b211
arethey <ardiederrayal06@gmail.com>
2023-03-24 20:08:06 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,148 @@
+'use client';
+// import Image from 'next/image';
+import useState from 'react-usestateref';
+
+enum Creator {
+  Me = 0,
+  Bot = 1
+}
+
+interface MessageProps {
+  text: string,
+  from: Creator,
+  key: number
+}
+
+interface InputProps {
+  onSend: (input: string) => void;
+  disabled: boolean;
+}
+
+const ChatMessage = ({text, from}:MessageProps) => {
+  return (
+    <>
+      {from === Creator.Me && (
+        <div className='bg-white p-4 rounded-lg flex gap-4 items-start whitespace-pre-wrap'>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-bounding-box" viewBox="0 0 16 16">
+              <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z"/>
+              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+            </svg>
+          </div>
+          <p className='text-gray-700'>{text}</p>
+        </div>
+      )}
+      {from === Creator.Bot && (
+        <div className='bg-gray-100 p-4 rounded-lg flex gap-4 items-start whitespace-pre-wrap'>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-robot" viewBox="0 0 16 16">
+              <path d="M6 12.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5ZM3 8.062C3 6.76 4.235 5.765 5.53 5.886a26.58 26.58 0 0 0 4.94 0C11.765 5.765 13 6.76 13 8.062v1.157a.933.933 0 0 1-.765.935c-.845.147-2.34.346-4.235.346-1.895 0-3.39-.2-4.235-.346A.933.933 0 0 1 3 9.219V8.062Zm4.542-.827a.25.25 0 0 0-.217.068l-.92.9a24.767 24.767 0 0 1-1.871-.183.25.25 0 0 0-.068.495c.55.076 1.232.149 2.02.193a.25.25 0 0 0 .189-.071l.754-.736.847 1.71a.25.25 0 0 0 .404.062l.932-.97a25.286 25.286 0 0 0 1.922-.188.25.25 0 0 0-.068-.495c-.538.074-1.207.145-1.98.189a.25.25 0 0 0-.166.076l-.754.785-.842-1.7a.25.25 0 0 0-.182-.135Z"/>
+              <path d="M8.5 1.866a1 1 0 1 0-1 0V3h-2A4.5 4.5 0 0 0 1 7.5V8a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v1a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1v-.5A4.5 4.5 0 0 0 10.5 3h-2V1.866ZM14 7.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.5A3.5 3.5 0 0 1 5.5 4h5A3.5 3.5 0 0 1 14 7.5Z"/>
+            </svg>
+          </div>
+          <p className='text-gray-700'>{text}</p>
+        </div>
+      )}
+    </>
+  )
+}
+
+const ChatInput = ({onSend, disabled}: InputProps) => {
+  const [input, setInput] = useState('');
+
+  const sendInput = () => {
+    onSend(input);
+    setInput('');
+  }
+
+  const handleKeyDown = (event: any) => {
+    if(event.keyCode === 13){
+      sendInput();
+    }
+  }
+
+  return (
+    <div className='bg-white border-2 p-2 rounded-lg flex gap-3 items-center justify-center'>
+      <input
+        value={input}
+        onChange={(ev: any) => setInput(ev.target.value)}
+        className="w-full py-2 px-3 text-gray-800 rounded-lg focus:outline-none"
+        type="text"
+        placeholder='Ask me anything'
+        disabled={disabled}
+        onKeyDown={(ev) => handleKeyDown(ev)}
+      />
+      {disabled && (
+        <div role="status">
+          <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          <span className="sr-only">Loading...</span>
+      </div>
+      )}
+      {!disabled && (
+        <button 
+        onClick={() => sendInput()}
+        className="p-2 rounded-md text-gray-500"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
+          </svg>
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default function Home() {
+  const [messages, setMessages, messagesRef] = useState<MessageProps[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const callApi = async (input: string) => {
+    setLoading(true);
+
+    const myMessage = {
+      text: input,
+      from: Creator.Me,
+      key: new Date().getTime()
+    };
+
+    setMessages([...messagesRef.current, myMessage]);
+    const response = await fetch('/api/generate-answer', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt: input
+      })
+    }).then(response => response.json());
+    setLoading(false);
+
+    if(response.text){
+      const botMessage: MessageProps = {
+        text: response.text,
+        from: Creator.Bot,
+        key: new Date().getTime()
+      }
+      setMessages([...messagesRef.current, botMessage]);
+    }
+  }
+
+  return (
+    <main className='relative max-w-2xl mx-auto'>
+      <div className='sticky top-0 w-full pt-10 px-4'>
+        <ChatInput onSend={input => callApi(input)} disabled={loading} />
+      </div>
+      <div className='mt-10 px-4'>
+        {messages.map((msg: MessageProps) => (
+          <ChatMessage key={msg.key} text={msg.text} from={msg.from} />
+        ))}
+        {messages.length === 0 && (
+          <p className='text-center text-gray-400'>I am at your service</p>
+        )}
+      </div>
+    </main>
+  )
+}
